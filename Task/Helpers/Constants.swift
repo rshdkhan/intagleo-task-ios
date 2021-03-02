0833161defb2a2dc92899fe12d56962bbe080dfb
Rashid Khan <rashid.khan@clustox.com>
2021-03-02 13:40:11 +0500

    image extension for rounding and contants file added.

---
+++ Task/Helpers/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Task
+//
+//  Created by Rashid Khan on 3/2/21.
+//
+
+import Foundation
+
+struct Constants {
+    static let headerNibName = "HomeTableViewHeader"
+    static let cellNibName = "HomeTableViewCell"
+    
+    static let textAscending = "Ascending"
+    static let textDescending = "Descending"
+    static let textCancel = "Cancel"
+    
+    static let searchImageName = "magnifyingglass"
+    static let filterImageName = "line.horizontal.3.decrease.circle"
+}
---
+++ Task/Helpers/extensions/Image.swift
@@ -0,0 +1,43 @@
+//
+//  Image.swift
+//  Task
+//
+//  Created by Rashid Khan on 3/2/21.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            let color = UIColor(cgColor: layer.borderColor!)
+            return color
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+}
